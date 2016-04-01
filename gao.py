55898c0ef8cc10d5b8c11f08cac3ede12e477321
Gladuo <me@gladuo.com>
2016-04-01 16:12:24 +0800

    add time.sleep()
    add time.sleep() to avoid rate limit

--- gao.py
+++ gao.py
@@ -3,6 +3,8 @@
 from pyquery import PyQuery as pq
 import requests
 import json
+import time
+import random
 
 TAGS_URL = 'https://book.douban.com/tag/?view=type&icn=index-sorttags-hot'
 API_SEARCH_URL = 'https://api.douban.com/v2/book/search'
@@ -38,7 +40,13 @@ def get_books_by_tag(tag='小说', start=0, count=100):
 
 
 def gao():
-    for i in range(0, 130000, 100):
+    lasti = 65900
+    for i in range(65900, 130000, 100):
+        if i-lasti >= 1000:
+            t = random.randint(10, 40)
+            print 'sleep for %ds' %t
+            time.sleep(t)
+            lasti = i
         get_books_by_tag('小说', i, 100)
 
 
